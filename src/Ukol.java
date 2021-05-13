7fb787d1694481f949c6446d962851f78ba77cf0
Pe2Ya <petii@SPSEJECNANET.CZ>
2021-05-13 10:36:49 +0200

    first commit

---
+++ src/Ukol.java
@@ -0,0 +1,13 @@
+
+public class Ukol {
+	private String ukol;
+	private Priorita priorita;
+
+	public Ukol(String ukol,Priorita priorita) {
+		this.priorita=priorita;
+		this.ukol=ukol;
+				
+		
+	}
+
+}
---
+++ src/Ukoly.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+
+public interface Ukoly {
+	
+	void pridatUkol(Ukol ukol);
+	Collection najdiUkol(String nazev);
+	
+
+}
